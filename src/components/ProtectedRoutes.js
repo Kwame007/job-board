eb046f4aca27c73561a5a9cc6f8a9e93bee3730b
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-29 21:46:30 +0000

    add protected routes

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import Home from "../pages/Home";
+import Headers from "./Header";
+import SignUp from "./Routes/SignUp";
 
 const ProtectedRoutes = () => {
   const auth = useSelector((state) => state.auth);
 
-  // if (!auth.isAuthenticated) {
-  //   return <div>You are not authenticated</div>;
-  // }
+  if (!auth.isAuthenticated) {
+    return <Navigate to="/signup" replace={true} />;
+  }
   console.log(auth);
   return (
     <>
-      <Home />
+      <Navigate to="/home" replace={true} />
     </>
   );
 };
