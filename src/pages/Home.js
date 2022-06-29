81286649700274adda9d18213641053ffee616f1
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-29 21:41:06 +0000

    add home & job pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Filter from "../components/Filter";
 import Header from "../components/Header";
+import JobCard from "../components/JobCard";
+import SearchInput from "../components/SearchInput";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <SearchInput />
+      <section className=" mt-10 gap-10 md:flex md:gap-20">
+        <div className=" md:w-2/5">
+          {" "}
+          <Filter />
+        </div>
+        <div className="md:w-full">
+          {" "}
+          <JobCard />
+        </div>
+      </section>
     </>
   );
 };
