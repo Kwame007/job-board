50d92018a7d6e6c2d80ad7eb401ce1a5777a6ce8
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-29 21:37:46 +0000

    remove commented code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import PageRoutes from "./components/Routes/PageRoutes";
+import SearchInput from "./components/SearchInput";
 
 function App() {
+  useEffect(() => {
+    // const host = "data.usajobs.gov";
+    // const userAgent = "darteyw@gmail.com";
+    // const authKey = "RUvksQfDicR3xPWTvlm3kkyEg5I940G03iWlalEYx5M=";
+    // const fetchData = async () => {
+    //   const response = await fetch("https://devitjobs.us/api/jobsLight");
+    //   const data = await response.json();
+    //   console.log(data);
+    // };
+
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-bg-blue h-fit">
+        <div className="container max-w-7xl mx-auto p-5">
+          <PageRoutes />
+        </div>
+      </div>
+    </>
   );
 }
 
