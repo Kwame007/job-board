4199914b5b3302104603f3b092bcc08b262586f2
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-29 16:45:59 +0000

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,28 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
       },
-
-      plugins: [],
+      backgroundImage: {
+        "bg-img":
+          "url('../src/assets/images/joel-ambass-9ewh3s3guV8-unsplash.jpg')",
+      },
+      colors: {
+        "text-gray": "#B9BDCF",
+        "text-blue": "#334680",
+      },
+      boxShadow: {
+        blue: "0px 2px 4px rgba(0, 0, 0, 0.05)",
+      },
+      backgroundColor: {
+        "bg-blue": "#f6f7fb",
+      },
+      height: {
+        147: "147px",
+        90: "90px",
+      },
+      width: {
+        90: "90px",
+      },
     },
+    plugins: [],
   },
 };
