b87cc7bcd1a7f44b9bb415d0ce31f59a82d7eae9
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-29 21:47:01 +0000

    add page routes

--- src/components/Routes/PageRoutes.js
+++ src/components/Routes/PageRoutes.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
-import Job from "../../pages/Job";
+import Job from "../../pages/Jobs";
+import Header from "../Header";
+import Login from "../Login";
 import ProtectedRoutes from "../ProtectedRoutes";
+import SignUp from "./SignUp";
 
 const PageRoutes = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* show limited number of jobs when not logged in */}
           <Route path="/" element={<ProtectedRoutes />} />
           <Route path="/job" element={<Job />} />
-          {/* <Route path="/protectedRoutes" element={<ProtectedRoutes />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
